698d9c59fccef5eee5c0a1bc89c1d97919b5b952
Yurai-Kitadate <yuurai18@gmail.com>
2022-04-25 15:14:05 +0900

    Initial Commit

---
+++ Shared/todoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppApp.swift
+//  Shared
+//
+//  Created by yurai on 2022/04/25.
+//
+
+import SwiftUI
+
+@main
+struct todoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
